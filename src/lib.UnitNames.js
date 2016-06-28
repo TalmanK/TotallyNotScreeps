5656ea7f610d2a40a04063fa0fa1991ab6de7fd0
Permentier Peter <peter.permentier@trasysgroup.com>
2016-06-28 12:11:34 +0200

    reworked Name Generation

--- src/lib.UnitNames.js
+++ src/lib.UnitNames.js
@@ -9,7 +9,7 @@
 
 var UnitNames =
 {
-    Generate: function()
+    GenerateGibberish: function()
     {
         var parts = ['ba','be','bi','bo','bu','da','de','di','do','du','fa','fe','fi','fo','fu','pa','pe','pi','po','pu'];
         var nbrparts = Math.random() * 3 + 1 ;
@@ -19,80 +19,20 @@ var UnitNames =
             name += parts[Math.floor(Math.random()*parts.length)]
         }
         return name.substring(0,1).toUpperCase() + name.substring(1,name.length)
+    },
+
+    Generate: function()
+    {
+      // Lists of first names and last names shamelessly stolen from the web
+      var firstNames = new Array("Allen","Bob","Carlton","David","Ernie","Foster","George","Howard","Ian","Jeffery","Kenneth","Lawrence","Michael","Nathen","Orson","Peter","Quinten","Reginald","Stephen","Thomas","Morris","Victor","Walter","Xavier","Charles","Anthony","Gordon","Percy","Conrad","Quincey","Armand","Jamal","Andrew","Matthew","Mark","Gerald","Alice","Bonnie","Cassie","Donna","Ethel","Grace","Heather","Jan","Katherine","Julie","Marcia","Patricia","Mabel","Jennifer","Dorthey","Mary Ellen","Jacki","Jean","Betty","Diane","Annette","Dawn","Jody","Karen","Mary Jane","Shannon","Stephanie","Kathleen","Emily","Tiffany","Angela","Christine","Debbie","Karla","Sandy","Marilyn","Brenda","Hayley","Linda")
+      var lastNames = new Array("Adams","Bowden","Conway","Darden","Edwards","Flynn","Gilliam","Holiday","Ingram","Johnson","Kraemer","Hunter","McDonald","Nichols","Pierce","Sawyer","Saunders","Schmidt","Schroeder","Smith","Douglas","Ward","Watson","Williams","Winters","Yeager","Ford","Forman","Dixon","Clark","Churchill","Brown","Blum","Anderson","Black","Cavenaugh","Hampton","Jenkins","Prichard")
+
+        var firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+        var lastName = firstNames[Math.floor(Math.random() * lastNames.length)];
+
+        return firstName + ' ' + lastName;
     }
+
 }
 
 module.exports = UnitNames;
-
-/*
-SAve this for later, if we decide to redo this:
-Lists for names found on the web
-
-
-var firstName = ["Runny", "Buttercup", "Dinky", "Stinky", "Crusty",
-"Greasy","Gidget", "Cheesypoof", "Lumpy", "Wacky", "Tiny", "Flunky",
-"Fluffy", "Zippy", "Doofus", "Gobsmacked", "Slimy", "Grimy", "Salamander",
-"Oily", "Burrito", "Bumpy", "Loopy", "Snotty", "Irving", "Egbert"];
-
-var middleName =["Waffer", "Lilly","Rugrat","Sand", "Fuzzy","Kitty",
- "Puppy", "Snuggles","Rubber", "Stinky", "Lulu", "Lala", "Sparkle", "Glitter",
- "Silver", "Golden", "Rainbow", "Cloud", "Rain", "Stormy", "Wink", "Sugar",
- "Twinkle", "Star", "Halo", "Angel"];
-
-var lastName1 = ["Snicker", "Buffalo", "Gross", "Bubble", "Sheep",
- "Corset", "Toilet", "Lizard", "Waffle", "Kumquat", "Burger", "Chimp", "Liver",
- "Gorilla", "Rhino", "Emu", "Pizza", "Toad", "Gerbil", "Pickle", "Tofu",
-"Chicken", "Potato", "Hamster", "Lemur", "Vermin"];
-var lastName2 = ["face", "dip", "nose", "brain", "head", "breath",
-"pants", "shorts", "lips", "mouth", "muffin", "butt", "bottom", "elbow",
-"honker", "toes", "buns", "spew", "kisser", "fanny", "squirt", "chunks",
-"brains", "wit", "juice", "shower"];
-
-var malename = new Array(
-"Allen","Bob","Carlton",
-"David","Ernie","Foster",
-"George","Howard","Ian",
-"Jeffery","Kenneth","Lawrence",
-"Michael","Nathen","Orson",
-"Peter","Quinten","Reginald",
-"Stephen","Thomas","Morris",
-"Victor","Walter","Xavier",
-"Charles","Anthony","Gordon",
-"Percy","Conrad","Quincey",
-"Armand","Jamal","Andrew",
-"Matthew","Mark","Gerald"
-)
-var femalename = new Array(
-"Alice","Bonnie","Cassie",
-"Donna","Ethel","Grace",
-"Heather","Jan","Katherine",
-"Julie","Marcia","Patricia",
-"Mabel","Jennifer","Dorthey",
-"Mary Ellen","Jacki","Jean",
-"Betty","Diane","Annette",
-"Dawn","Jody","Karen",
-"Mary Jane","Shannon","Stephanie",
-"Kathleen","Emily","Tiffany",
-"Angela","Christine","Debbie",
-"Karla","Sandy","Marilyn",
-"Brenda","Hayley","Linda"
-)
-var lastname = new Array(
-"Adams","Bowden","Conway",
-"Darden","Edwards","Flynn",
-"Gilliam","Holiday","Ingram",
-"Johnson","Kraemer","Hunter",
-"McDonald","Nichols","Pierce",
-"Sawyer","Saunders","Schmidt",
-"Schroeder","Smith","Douglas",
-"Ward","Watson","Williams",
-"Winters","Yeager","Ford",
-"Forman","Dixon","Clark",
-"Churchill","Brown","Blum",
-"Anderson","Black","Cavenaugh",
-"Hampton","Jenkins","Prichard"
-)
-
-
-
-*/
