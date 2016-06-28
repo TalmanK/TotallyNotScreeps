4f03db2e6c0dcdec18264b10b0f6044ec787f09c
Permentier Peter <peter.permentier@trasysgroup.com>
2016-06-28 11:49:48 +0200

    filename change

--- core.states.js
+++ core.states.js
@@ -3,7 +3,7 @@
  * module.exports.thing = 'a thing';
  *
  * You can import it from another modules like this:
- * var mod = require('core.states');
+ * var mod = require('core.States');
  * mod.thing == 'a thing'; // true
  */
 
