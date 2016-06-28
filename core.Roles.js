8d3980650c819ab476f0239be2ee10827521ef24
Permentier Peter <peter.permentier@trasysgroup.com>
2016-06-28 11:48:06 +0200

    Removed obsolete spawner role

--- core.Roles.js
+++ core.Roles.js
@@ -11,7 +11,6 @@ module.exports = {
     upgrader: require('role.Upgrader'),
     harvester: require('role.Harvester'),
     builder: require('role.Builder'),
-    spawner: require('role.Spawner'),
     miner: require('role.Miner'),
     archer: require('role.Archer'),
     hauler: require('role.Hauler')
