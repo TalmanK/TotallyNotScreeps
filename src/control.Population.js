6b90adc40b9557a8f358c448d81bbbfc9b83d3b5
TalmanK <talman.khan@gmail.com>
2016-06-30 20:14:08 +0200

    cleanup of code warnings

--- src/control.Population.js
+++ src/control.Population.js
@@ -24,7 +24,7 @@ Population.prototype.run = function ()
 
     // Run the creeps
     var roles = require('core.Roles');
-    for(var name in this.creeps) { roles[this.creeps[name].memory.role].run(this.creeps[name]); }
+    for(let name in this.creeps) { roles[this.creeps[name].memory.role].run(this.creeps[name]); }
 
     // Try to spawn the next creep in the line.
     var nextSpawn = this.getNextSpawn();
@@ -32,7 +32,7 @@ Population.prototype.run = function ()
     {
         this.SpawnNewCreep(nextSpawn);
     }
-}
+};
 
 
 Population.prototype.getCreepDistribution = function ()
@@ -61,7 +61,7 @@ Population.prototype.getCreepDistribution = function ()
 
     return creepDistribution;
 
-}
+};
 
 
 Population.prototype.SpawnNewCreep = function (spawnType)
@@ -95,16 +95,16 @@ Population.prototype.SpawnNewCreep = function (spawnType)
     {
         console.log('role.spawner: SpawnNewCreep: [WARNING] - Unknown Creep Class: ' + spawnType.creepType)
     }
-}
+};
 
 Population.prototype.getNextSpawn =  function ()
 {
     //What should we spawn next.
     var nextSpawns = [] ;
-    var spawnPriority = 0
+    var spawnPriority = 0;
 
     // ----------------------------
-    spawnPriority = 1  // (lower is better)
+    spawnPriority = 1;  // (lower is better)
     // ----------------------------
 
     // if we have no creeps. Spawn in a harvester
@@ -122,12 +122,12 @@ Population.prototype.getNextSpawn =  function ()
     }
 
     // ----------------------------
-    spawnPriority = 2  // (lower is better)
+    spawnPriority = 2;  // (lower is better)
     // ----------------------------
 
 
     // ----------------------------
-    spawnPriority = 3  // (lower is better)
+    spawnPriority = 3; // (lower is better)
     // ----------------------------
 
     // Try to have equal haulers & miners
@@ -144,7 +144,7 @@ Population.prototype.getNextSpawn =  function ()
     }
 
     // ----------------------------
-    spawnPriority = 4  // (lower is better)
+    spawnPriority = 4;  // (lower is better)
     // ----------------------------
 
     // if we have no upgraders, spawn one
@@ -161,7 +161,7 @@ Population.prototype.getNextSpawn =  function ()
     }
 
     // ----------------------------
-    spawnPriority = 9  // (lower is better)
+    spawnPriority = 9;  // (lower is better)
     // ----------------------------
     //Try out a few archers
     // nextSpawns.push ( {spawnPriority: spawnPriority, creepType: 'AXE', creepRole: 'archer'} );
@@ -174,7 +174,7 @@ Population.prototype.getNextSpawn =  function ()
     // console.log('getNextSpawn: sorted (next): ' + JSON.stringify(tmp[0]) );
 
     return nextSpawns[0];
-}
+};
 
 Population.prototype.Report =  function ()
 {
@@ -191,7 +191,7 @@ Population.prototype.Report =  function ()
     {
         line += ' (' + n + '): ' + this.creepDistribution.roles[n];
     }
-    report.push('-     Roles: ' + line )
+    report.push('-     Roles: ' + line );
 
     var n = this.getNextSpawn();
     if (n !== undefined)
@@ -205,7 +205,7 @@ Population.prototype.Report =  function ()
 
 
     return report;
-}
+};
 
 module.exports = Population ;
 
