ad0f59f02aed7e6e148a131a0fc2cd75f175b774
TalmanK <talman.khan@gmail.com>
2016-07-04 11:43:56 +0200

    Should now also deposit in containers

--- src/state.DepositEnergy.js
+++ src/state.DepositEnergy.js
@@ -1,24 +1,61 @@
 /*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
+ * Deposit Energy State
  *
- * You can import it from another modules like this:
- * var mod = require('state.DepositEnergy');
- * mod.thing == 'a thing'; // true
+ * Should try to deposit in structures that are nto at max capacity.
+ * In the following order:
+ * 1) STRUCTURE_SPAWN       (energy & energyCapacity)
+ * 2) STRUCTURE_EXTENSION   (energy & energyCapacity)
+ * 3) STRUCTURE_TOWER       (energy & energyCapacity)
+ * 4) STRUCTURE_CONTAINER   (store & storeCapacity)
  */
 
 module.exports = {
 run: function (creep, newState, altState)
 {
+    // First, find spawns with remaining capacity.
     var targets = creep.room.find(FIND_STRUCTURES,
     {
             filter: (structure) =>
             {
-                return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                return (structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
             }
     });
+
+    // If no valid spawns, look for extensions
+    if(targets.length > 0)
+    {
+        targets = creep.room.find(FIND_STRUCTURES,
+            {
+                filter: (structure) =>
+                {
+                    return (structure.structureType == STRUCTURE_EXTENSION) && structure.energy < structure.energyCapacity;
+                }
+            });
+    }
+
+    // If no valid extensions, look for towers
+    if(targets.length > 0)
+    {
+        targets = creep.room.find(FIND_STRUCTURES,
+            {
+                filter: (structure) =>
+                {
+                    return (structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                }
+            });
+    }
+
+    // If no valid towers, look for containers (beware the different store)
+    if(targets.length > 0)
+    {
+        targets = creep.room.find(FIND_STRUCTURES,
+            {
+                filter: (structure) =>
+                {
+                    return (structure.structureType == STRUCTURE_CONTAINER) && _.sum(structure.store) < structure.storeCapacity;
+                }
+            });
+    }
 
     if(targets.length > 0)
     {
@@ -31,13 +68,13 @@ run: function (creep, newState, altState)
     else
     {
         // No valid targets for deposits, perform alternate tasks
-        creep.memory.state = altState
+        creep.memory.state = altState;
         creep.say(creep.memory.state);
     }
     if(creep.carry.energy == 0)
     {
         //No more energy, switch to next state
-        creep.memory.state = newState
+        creep.memory.state = newState;
         creep.say(creep.memory.state);
     }
 }
